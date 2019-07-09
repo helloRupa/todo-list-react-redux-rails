e21aea6f377098ea10a5bfbe85d457f53d512ee5
Rupa Dhillon <rupa211984@gmail.com>
2019-07-08 18:31:45 -0700

    Remove unused actions

--- frontend/components/todo_list/todo_detail_view.jsx
+++ frontend/components/todo_list/todo_detail_view.jsx
@@ -5,7 +5,7 @@ import StepListContainer from '../step_list/step_list_container';
 
 class TodoDetailView extends React.Component {
   render() {
-    const { todo, removeTodo, deleteTodo } = this.props;
+    const { todo, deleteTodo } = this.props;
     return(
       <div>
         <p className="todo-body">{ todo.body }</p>
