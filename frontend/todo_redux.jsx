dc3a07148bc1989e92c7be7040d6b921685c1c48
Rupa Dhillon <rupa211984@gmail.com>
2019-07-10 00:47:15 -0700

    Remove global vars

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -5,12 +5,8 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const preloadedState = localStorage.state ?
-  //   JSON.parse(localStorage.state) : {};
-  let store = configureStore(/*preloadedState*/);
-  
-// DELETE, FOR TESTING ONLY
-window.store = store;
+  let store = configureStore();
   const root = document.getElementById('content');
+
   ReactDOM.render(<Root store={store} />, root);
 });
